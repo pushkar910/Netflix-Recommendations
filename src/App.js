b35ee02ac80b600e9aad703e11d2af3c44000071
Pushkar Kumar <kmrpushkar910@gmail.com>
2021-11-10 23:29:13 +0530

    Project created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./Card";
+import "./index.css";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading_style">My Top 5 Netflix Series Recommendation</h1>
+      {data.map((movie) => {
+        return (
+          <Card
+            imgsrc={movie.imgsrc}
+            title={movie.title}
+            name={movie.name}
+            link={movie.link}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    name: "DARK",
+    imgsrc:
+      "https://w0.peakpx.com/wallpaper/922/842/HD-wallpaper-dark-netflix-poster-dark-netflix-hbo-jonas-jonas-kahnwald-manipulation.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    name: "Stranger Things",
+    imgsrc:
+      "https://i.pinimg.com/originals/eb/21/31/eb213187c5ebf158ffc7b21e865e5112.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+  {
+    name: "BODYGUARD",
+    imgsrc:
+      "https://i.pinimg.com/originals/ee/71/7b/ee717b752163182e48d0ee7dfc771823.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80235864",
+  },
+  {
+    name: "House Of Cards",
+    imgsrc:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/House_of_Cards_season_2.png/250px-House_of_Cards_season_2.png",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/70178217",
+  },
+  {
+    name: "The SPY",
+    imgsrc:
+      "https://i.pinimg.com/736x/36/a7/5b/36a75be5f831141bf560f0d8a9498ed6.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/title/80178151",
+  },
+];
+
+export default data;
